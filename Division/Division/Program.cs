6d561394a43855535553670bd649a49bb0c97c5f
tomneozed <tomneozed@gmail.com>
2023-09-08 10:41:54 +0200

    Exercice 4 - Gestion des erreurs

---
+++ Division/Division/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal divise = 0;
+            decimal diviseur = 0;
+            decimal resultat = 0;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Veuillez entrer une valeur à diviser : ");
+                    divise = decimal.Parse(Console.ReadLine());
+                    Console.Write("Veuillez entrer une autre valeur par laquelle diviser : ");
+                    diviseur = decimal.Parse(Console.ReadLine());
+
+                    try
+                    {
+                        resultat = divise/diviseur;
+                       
+                    }catch(DivideByZeroException ex)
+                    {
+                        Console.WriteLine("x/0 = infini et ça rentre pas dans un int");
+                    }
+
+                }
+                catch (FormatException ex)
+                {
+                    // Gestion des non-entiers
+                    Console.WriteLine("Veuillez rentrer des entiers");
+                }
+                Console.WriteLine("Résultat : " + resultat);
+
+            } while (resultat == 0);
+        }
+    }
+}
---
+++ GestionErreur/GestionErreur/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionErreur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int reponse = 0;
+            do
+            {
+                try
+                {
+                    Console.Write("Veuillez entrer une valeur entre 1 et 3 : ");
+                    reponse = int.Parse(Console.ReadLine());
+
+                }catch (FormatException ex)
+                {
+                    Console.WriteLine("Veuillez rentrer un entier compris entre 1 et 3");
+                }
+
+            } while (!reponse.Equals(1) && !reponse.Equals(2) && !reponse.Equals(3));
+            int laValeur = 0;
+
+            switch (reponse)
+            {
+                case 1:
+                    goto default;
+                    break;
+                case 2:
+                    laValeur += 25;
+                    break;
+                case 3:
+                    laValeur += 30;
+                    goto default;
+                default:
+                    laValeur += 10;
+                    break;
+            }
+            Console.WriteLine("La valeur est : " + laValeur);
+        }
+    }
+}

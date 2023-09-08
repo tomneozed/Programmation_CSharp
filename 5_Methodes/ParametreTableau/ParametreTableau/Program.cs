d88abb5c61140134a2be386258463cbaa0c3d99a
tomneozed <tomneozed@gmail.com>
2023-09-08 13:02:21 +0200

    5 Methodes - Exercice 4 - Parametre Tableau

---
+++ 5_Methodes/ParametreTableau/ParametreTableau/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametreTableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tableau = { "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche" };
+
+            string[,] tableau2 = {
+                { "lundi", "monday" },
+                { "mardi", "tuesday" },
+                { "mercredi", "wednesday" },
+                { "jeudi", "thursday" },
+                { "vendredi", "friday" },
+                { "samedi", "saturday" },
+                { "dimanche", "sunday" }
+            };
+
+            foreach (string item in tableau)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < tableau2.GetLength(0); i++)
+            {
+                for (int j = 0; j < tableau2.GetLength(1); j++)
+                {
+                    Console.Write(tableau2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

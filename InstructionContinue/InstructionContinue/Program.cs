2135bfdbcd828fcc9ca745a227a3744f6b2d478e
tomneozed <tomneozed@gmail.com>
2023-09-08 10:10:12 +0200

    Exercice 3 - Instruction Continue

---
+++ InstructionContinue/InstructionContinue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstructionContinue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (i > 8)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

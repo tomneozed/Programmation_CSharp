146e2084d0417298a3053c9264b77d8176d220e9
tomneozed <tomneozed@gmail.com>
2023-09-08 13:29:26 +0200

    6 Objets, les bases - Exercice 2 - Portee

---
+++ 6_Objets_bases/Portee/Portee/MaBoucle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portee
+{
+    public class MaBoucle
+    {
+        public int i { get; set; }
+        public void boucle(int valeur)
+        {
+            for (i = 0; i < valeur; i++)
+            {
+            }
+        }
+    }
+}
---
+++ 6_Objets_bases/Portee/Portee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+
+            }
+            Console.WriteLine(i);
+
+            // -------------------------------------
+
+            int j;
+            boucle(20, out j);
+            Console.WriteLine(j);
+
+            // -------------------------------------
+
+            MaBoucle maBoucle = new MaBoucle();
+            maBoucle.boucle(20);
+            Console.WriteLine(maBoucle.i);
+        }
+        static void boucle(int valeur, out int i)
+        {
+            for (i = 0; i < valeur; i++)
+            {
+
+            }
+        }
+    }
+}

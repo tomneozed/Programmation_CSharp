54ca07a7b7634d006699ac93a46e12984437f651
tomneozed <tomneozed@gmail.com>
2023-09-08 13:14:21 +0200

    6 Objets, les bases - Exercice 1 - Produit

---
+++ 6_Objets_bases/CreationObjet/CreationObjet/Produit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationObjet
+{
+    internal class Produit
+    {
+        public string nom; //nom du produit
+        public uint quantite; // quantité superieure a 0
+        decimal montant; // montant superieur a 0
+
+        public void setMontant(decimal montant)
+        {
+            if (montant < 0)
+            {
+                Console.WriteLine("please enter a positive value");
+            } else
+            {
+                this.montant = montant;
+            }
+        }
+
+        public decimal getMontant()
+        {
+            return this.montant;
+        }
+    }
+}
---
+++ 6_Objets_bases/CreationObjet/CreationObjet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationObjet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produit produit = new Produit();
+            produit.nom = "Voyage";
+            produit.quantite = 10;
+            produit.setMontant(1);
+
+            Console.WriteLine(produit.nom);
+            Console.WriteLine(produit.quantite);
+            Console.WriteLine(produit.getMontant());
+        }
+    }
+}

8034a66247efb66a6417e75ee5feb34614804af8
tomneozed <tomneozed@gmail.com>
2023-09-11 15:15:38 +0200

    8 Objets C# - Exercice 1

---
+++ 8_Objets_C#/Garage/Garage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture voiture1 = new Voiture("Clio", "Renault", "Vert");
+            Console.WriteLine("La {0} {1} est de couleur {2}", 
+                voiture1.Marque,
+                voiture1.Modele,
+                voiture1.Couleur
+            );
+
+            voiture1.Couleur = "Bleu";
+            Console.WriteLine("La {0} {1} est de couleur {2}",
+                voiture1.Marque,
+                voiture1.Modele,
+                voiture1.Couleur
+            );
+
+            voiture1.Roule();
+        }
+    }
+}
---
+++ 8_Objets_C#/Garage/Garage/Voiture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Voiture
+    {
+        public string Modele {  get; set; }
+        public string Marque {  get; set; }
+        public string Couleur {  get; set; }
+
+        public Voiture() { }
+        public Voiture(string modele, string marque, string couleur) {
+            this.Modele = modele;
+            this.Marque = marque;
+            this.Couleur = couleur;
+        }
+
+        public void Roule() {
+            Console.WriteLine("La voiture roule !");
+        }
+
+    }
+}

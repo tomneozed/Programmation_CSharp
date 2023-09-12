33c34de8b6b9790e75b99959638921e90aba0ac8
tomneozed <tomneozed@gmail.com>
2023-09-12 07:59:44 +0200

    9 Délégués & Evènements - Exercice 1

---
+++ 9_Delegues_et_evenements/DelegueSimple/DelegueSimple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegueSimple
+{
+    public delegate void SimpleDelegue();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DebutFacture();
+            FinFacture();
+            SimpleDelegue Info = new SimpleDelegue(DebutFacture);
+            Info();
+
+            Info = FinFacture;
+            Info();
+        }
+
+        public static void DebutFacture()
+        {
+            Console.WriteLine("Début de facturation");
+        }
+
+        public static void FinFacture()
+        {
+            Console.WriteLine("Fin de facturation");
+        }
+
+    }
+}

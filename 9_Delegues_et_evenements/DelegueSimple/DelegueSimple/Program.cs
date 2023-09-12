ec2b54aa2e78fbde2baba9041820f00247c1212b
tomneozed <tomneozed@gmail.com>
2023-09-12 08:07:13 +0200

    9 Délégués & Evènements - Exercice 2

--- 9_Delegues_et_evenements/DelegueSimple/DelegueSimple/Program.cs
+++ 9_Delegues_et_evenements/DelegueSimple/DelegueSimple/Program.cs
@@ -8,10 +8,14 @@ using System.Threading.Tasks;
 namespace DelegueSimple
 {
     public delegate void SimpleDelegue();
+
+    public delegate void AfficheMessage(string message);
+
     internal class Program
     {
         static void Main(string[] args)
         {
+/*
             DebutFacture();
             FinFacture();
             SimpleDelegue Info = new SimpleDelegue(DebutFacture);
@@ -19,6 +23,12 @@ namespace DelegueSimple
 
             Info = FinFacture;
             Info();
+*/
+            // --------------------------------------------------------------
+
+            AfficheMessage messager = new AfficheMessage(Info);
+            Processus(messager);
+
         }
 
         public static void DebutFacture()
@@ -29,6 +39,22 @@ namespace DelegueSimple
         public static void FinFacture()
         {
             Console.WriteLine("Fin de facturation");
+        }
+
+        static void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Processus(AfficheMessage Affichage) {
+            if (Affichage != null)
+            {
+                Affichage("Début de la facturation");
+            }
+            if (Affichage != null)
+            {
+                Affichage("Facture Terminée");
+            }
         }
 
     }
